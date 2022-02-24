f418c0b860b1ba4af75bc594d8ad767f30839221
eliasdistrict <elias.elghandourmaalouf@lau.edu>
2022-02-24 20:53:08 +0200

    First part of project

---
+++ app/Http/Resources/Product/ProductCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductCollection extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'SKU' => $this->discount,
+            'name' => $this->name,
+            'image' => $this->image,
+            'description' => $this->detail,
+             
+            'details' => [
+                'price' => $this->price,
+                'quantity' => $this->stock, 
+            ]     
+        ];
+    }
+}
